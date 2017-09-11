2b58a5f4eced62dda88ded8af2d221e57d8a09fc
pzab <pavlo.zabudskyi@gmail.com>
2017-09-11 18:24:02 +0300

    fix startup-script.sh; modify README.md with startup script support

--- startup-script.sh
+++ startup-script.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-bash install_ruby.sh
-bash deploy.sh
-bash install_mongodb.sh
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt update && sudo apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
+su appuser <<EOF
+cd ~
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm requirements
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
+git clone https://github.com/Artemmkin/reddit.git
+cd reddit && bundle install
+puma -d
+EOF
