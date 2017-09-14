969691d460be9ffdf8a4991b720666382822312a
pzab <pavlo.zabudskyi@gmail.com>
2017-09-14 12:14:18 +0300

    add set -e in install_mongodb.sh and install_ruby.sh scripts

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Run this script with sudo
+set -e
 
 if [[ $(id -u) -ne 0 ]]; then
   echo "Please run script as root"
@@ -7,7 +8,7 @@ if [[ $(id -u) -ne 0 ]]; then
 fi
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt update && sudo apt-get install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
