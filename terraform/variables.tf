a624b7ad605f82b4f92871e3c67af52d252d4a76
pzab <pavlo.zabudskyi@gmail.com>
2017-09-17 12:31:06 +0300

    prepare terraform project for module structure

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,13 @@ variable private_key_path {
 variable disk_image {
   description = "Disk image"
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-db-base"
+}
